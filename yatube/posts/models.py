80ccc24a23d3f09b462ce8c062d50f20d07411b7
Maxim Nemkov <dedau@bk.ru>
2021-11-22 00:34:27 +0300

    minor fixes for PEP8 in settings and test

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         related_name='posts'
     )
@@ -17,6 +18,7 @@ class Post(models.Model):
         null=True,
         on_delete=models.CASCADE,
     )
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)

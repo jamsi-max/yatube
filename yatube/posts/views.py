b9fe7cdadab4c3f39379dc8fedc76dcf110b4dc3
Maxim Nemkov <dedau@bk.ru>
2021-11-23 22:38:40 +0300

    finish fixed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Post, Group
 
 
 def index(request):
     tempate = 'posts/index.html'
-    title = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
-        'title': title,
         'posts': posts,
     }
     return render(request, tempate, context)
@@ -16,11 +15,9 @@ def index(request):
 
 def group_posts(request, slug):
     tempate = 'posts/group_list.html'
-    title = f'Записи сообщества {slug}.'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:10]
     context = {
-        'title': title,
         'posts': posts,
     }
     return render(request, tempate, context)
